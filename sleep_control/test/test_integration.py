625365de43a9f6a8e5fc40788d30ccbe4a350c6e
admin-326 <liujingchu@gmail.com>
2016-03-26 04:03:37 +0800

    test_integration update

--- sleep_control/test/test_integration.py
+++ sleep_control/test/test_integration.py
@@ -11,7 +11,8 @@ from qlearning.qtable import QAgent
 pd.set_option('mode.chained_assignment', None)
 
 # Setting up data
-session_df = pd.read_csv(filepath_or_buffer='../data/net_traffic_nonull_sample.dat', sep=',', names=['uid','location','startTime_unix','duration_ms','domainProviders','domainTypes','domains','bytesByDomain','requestsByDomain'])
+print "Reading data..."
+session_df = pd.read_csv(filepath_or_buffer='../data/net_traffic_nonull.dat', sep=',', names=['uid','location','startTime_unix','duration_ms','domainProviders','domainTypes','domains','bytesByDomain','requestsByDomain'])
 session_df.index.name = 'sessionID'
 session_df['endTime_unix'] = session_df['startTime_unix'] + session_df['duration_ms']
 session_df['startTime_datetime'] = pd.to_datetime(session_df['startTime_unix'], unit='ms')  # convert start time to readible date_time strings
@@ -22,6 +23,7 @@ session_df.sort(['startTime_datetime'], ascending=True, inplace=True)  # get it 
 session_df['interArrivalDuration_datetime'] = session_df.groupby('location')['startTime_datetime'].diff()  # group-wise diff
 session_df['interArrivalDuration_ms'] = session_df.groupby('location')['startTime_unix'].diff()  # group-wise diff
 
+print "Setting up Emulation environment..."
 # Setting up Emulation
 te = TrafficEmulator(session_df=session_df, time_step=pd.Timedelta(minutes=1))
 ts = TrafficServer()
@@ -30,6 +32,7 @@ agent = QAgent(actions=actions, alpha=0.5, gamma=0.5, explore_strategy='epsilon'
 c = QTableController(agent=agent)
 emu = Emulation(te=te, ts=ts, c=c)
 
+print "Emulation starting"
 # run...
 while emu.epoch is not None:
     t = time.time()
