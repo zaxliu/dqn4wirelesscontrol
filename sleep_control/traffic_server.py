0b275b269989b9b2517213d0a69feb71d9f9b5f5
Lewis-VM <liujingchu@gmail.com>
2016-03-25 21:18:28 -0700

    re-structure TrafficServer.serve_requests_()

--- sleep_control/traffic_server.py
+++ sleep_control/traffic_server.py
@@ -21,10 +21,8 @@ class TrafficServer:
     requests. These operational states are reflected in both the service generated and the cost emitted.
     """
     def __init__(self, verbose=0):
-        self.epoch = None
-        self.current_traffic = None
-        self.q = None
-        self.reset()
+        self.epoch = 0
+        self.q = pd.DataFrame(columns=['sessionID', 'uid', 'arriveTime_epoch', 'bytesSent_per_request_per_domain'])
         self.verbose = verbose      # verbosity level
 
     # Public Methods
@@ -34,9 +32,15 @@ class TrafficServer:
         """
         if traffic_df is None:
             raise ValueError("Please feed traffic.")
-        self.current_traffic = traffic_df
-        traffic_observation = self.observe_traffic_()
-        q_observation = self.observe_q_()
+
+        # Enqueue traffic
+        traffic_df_cp = traffic_df.copy()
+        traffic_df_cp['arriveTime_epoch'] = self.epoch
+        self.q = self.q.append(traffic_df_cp, ignore_index=True)
+
+        # Compile observation
+        traffic_observation = self.observe_traffic_(traffic_df_cp)
+        q_observation = self.observe_q_(self.q)
         return traffic_observation, q_observation
 
     def get_service_and_cost(self, control):
@@ -44,56 +48,46 @@ class TrafficServer:
         """
 
         sleep_flag, control_req = control  # extract control commands
-        self.enqueue_all_traffic_()  # put all incoming requests into queue first
-        # if server sleep in the current round,
         if sleep_flag:
             service = pd.DataFrame(columns=['sessionID', 'service_per_request_per_domain'])
-            cost = 0  # set cost=0
-        # else...
+            cost = 0
         else:
             service = self.serve_requests_(control_req)
-            cost = 1  # set cost=1
-        # Prepare for next epoch
-        self.current_traffic = None  # clear traffic buffer
+            cost = 1
         self.epoch += 1
         return service, cost
 
     def reset(self):
         self.epoch = 0
-        self.current_traffic = None
         self.q = pd.DataFrame(columns=['sessionID', 'uid', 'arriveTime_epoch', 'bytesSent_per_request_per_domain'])
 
     # Private Methods
-    def observe_traffic_(self):
+    @staticmethod
+    def observe_traffic_(traffic_df):
         """Summary information from current traffic
         Currently the total number of requests and bytes (tuple)
         """
         num_req = 0
         num_bytes = 0
-        for idx in self.current_traffic.index:
-            bytesSent_req_domain = json.loads(self.current_traffic.loc[idx, 'bytesSent_per_request_per_domain'])
+        for idx in traffic_df.index:
+            bytesSent_req_domain = json.loads(traffic_df.loc[idx, 'bytesSent_per_request_per_domain'])
             num_req += sum([len(bytesSent_req_domain[domain]) for domain in bytesSent_req_domain])
             num_bytes += sum([bytesSent_req_domain[domain][reqID]
                               for domain in bytesSent_req_domain for reqID in bytesSent_req_domain[domain]])
         return num_req, num_bytes
 
-    def observe_q_(self):
+    @staticmethod
+    def observe_q_(q):
         """Summary queue state for observation
 
         Currently the summary is the # of requests left in to queue
         :return: queue observation
         """
         q_len = 0
-        for idx in self.q.index:
-            bytesSent_req_domain = json.loads(self.q.loc[idx, 'bytesSent_per_request_per_domain'])
+        for idx in q.index:
+            bytesSent_req_domain = json.loads(q.loc[idx, 'bytesSent_per_request_per_domain'])
             q_len += sum([len(bytesSent_req_domain[domain]) for domain in bytesSent_req_domain])
         return q_len
-
-    def enqueue_all_traffic_(self):
-        enq_df = self.current_traffic
-        enq_df['arriveTime_epoch'] = self.epoch
-        self.q = self.q.append(enq_df, ignore_index=True)
-        return
 
     def dequeue_all_traffic_(self):
         q = self.q
@@ -107,130 +101,81 @@ class TrafficServer:
         :return:
         """
         service_df = pd.DataFrame(columns=['sessionID', 'service_per_request_per_domain'])
-        service_str = control_req  # service command for queued and incoming requests
-        q = None
-        # 1. serve all queued requests
-        if service_str == 'serve_all':
-            q = self.dequeue_all_traffic_()  # deque all queued traffic
-            for idx in q.index:  # for each traffic entry
-                sessionID = int(q.loc[idx, 'sessionID'])
-                bytesSent_req_domain = json.loads(q.loc[idx, 'bytesSent_per_request_per_domain'])
+        drop_indices = []
+        num_req_serve = 0
+        num_req_queue = 0
 
-                # register service for each request
-                service_req_domain = {}
-                for domain in bytesSent_req_domain:  # for each domain in the entry
-                    for reqID in bytesSent_req_domain[domain]:  # for each request under the domain
-                        # register 'serve' to the service
-                        if domain not in service_req_domain:
-                            service_req_domain[domain] = {}
-                        service_req_domain[domain][reqID] = 'serve'
-                q.loc[idx, 'service_per_request_per_domain'] = json.dumps(service_req_domain)
+        # iterate through q, append to service_df, merge duplicated session ID, update q
+        for idx in self.q.index:
+            sessionID = int(self.q.loc[idx, 'sessionID'])
+            bytesSent_req_domain = json.loads(self.q.loc[idx, 'bytesSent_per_request_per_domain'])
+            service_req_domain, bytesSent_req_domain_updated, num_req_serve_row, num_req_queue_row = self.serve_row_(
+                control_req=control_req, bytesSent_req_domain=bytesSent_req_domain
+            )
+            # append to service_df (with dedup)
+            service_df = self.append_service_row(service_df=service_df, sessionID=sessionID, service_req_domain=service_req_domain)
+            # update queue
+            if num_req_queue_row == 0:
+                drop_indices.append(idx)
+            else:
+                self.q.loc[idx, 'bytesSent_req_domain'] = json.dumps(bytesSent_req_domain_updated)
+            # update counter
+            num_req_serve += num_req_serve_row
+            num_req_queue += num_req_queue_row
+        self.q.drop(drop_indices, inplace=True)
 
-                # if multiple entries with same sessionID exist, merge into one entry
-                idx_old = (int(sessionID) == q['sessionID']).nonzero()[0][0]
-                if idx != idx_old:
-                    print idx,
-		    print idx_old
-		    service_req_domain_old = json.loads(q.loc[idx_old, 'service_per_request_per_domain'])
-                    for domain in service_req_domain:
-                        for reqID in service_req_domain[domain]:
-                            if domain not in service_req_domain_old:
-                                service_req_domain_old[domain] = {}
-                            if reqID not in service_req_domain_old[domain]:
-                                service_req_domain_old[domain][reqID] = service_req_domain[domain][reqID]
-                    q.loc[idx_old, 'service_per_request_per_domain'] = json.dumps(service_req_domain_old)
-                    q.drop([idx], inplace=True)
-
-            if self.verbose > 0:
+        # Verbose message
+        if self.verbose > 0:
+            if control_req == 'serve_all':
                 print "TrafficServer.__serve_requests__(): serving all requests in queue."
-
-        # 2. keep all requests in queue
-        elif service_str == 'queue_all':
-            q = self.q.copy()
-            for idx in q.index:
-                sessionID = int(q.loc[idx, 'sessionID'])
-                bytesSent_req_domain = json.loads(q.loc[idx, 'bytesSent_per_request_per_domain'])
-
-                # register service for each request
-                service_req_domain = {}
-                for domain in bytesSent_req_domain:
-                    for reqID in bytesSent_req_domain[domain]:
-                        if domain not in service_req_domain:
-                            service_req_domain[domain] = {}
-                        service_req_domain[domain][reqID] = 'queue'
-                q.loc[idx, 'service_per_request_per_domain'] = json.dumps(service_req_domain)
-
-                # if multiple entries with same sessionID exist, merge into one entry
-                idx_old = (int(sessionID) == q['sessionID']).nonzero()[0][0]
-                if idx != idx_old:
-                    print idx,
-                    print idx_old
-                    service_req_domain_old = json.loads(q.loc[idx_old, 'service_per_request_per_domain'])
-                    for domain in service_req_domain:
-                        for reqID in service_req_domain[domain]:
-                            if domain not in service_req_domain_old:
-                                service_req_domain_old[domain] = {}
-                            service_req_domain_old[domain][reqID] = service_req_domain[domain][reqID]
-                    q.loc[idx_old, 'service_per_request_per_domain'] = json.dumps(service_req_domain_old)
-                    q.drop([idx], inplace=True)
-
-            if self.verbose > 0:
+            elif control_req == 'queue_all':
                 print "TrafficServer.__serve_requests__(): queuing all requests in queue."
+            elif control_req == 'random_serve_and_queue':
+                print "TrafficServer.__serve_requests__(): serving {} request and queueing {} requests.".format(
+                    num_req_serve, num_req_queue)
+            else:
+                print "TrafficServer.__serve_requests__(): control command not understood, return empty service_df."
+        return service_df
 
-        # 3. randomly serve or keep queuing requests
-        elif service_str == 'random_serve_and_queue':
-            q = self.q.copy()
-            num_serve = 0
-            num_queue = 0
-            for idx in q.index:
-                sessionID = int(q.loc[idx, 'sessionID'])
-                bytesSent_req_domain = json.loads(q.loc[idx, 'bytesSent_per_request_per_domain'])
-
-                # if multiple entries with same sessionID exist, merge into one entry
-                service_req_domain = {}
-                for domain in bytesSent_req_domain:
-                    for reqID in bytesSent_req_domain[domain]:
-                        if domain not in service_req_domain:
-                            service_req_domain[domain] = {}
-                        r = np.random.rand()
-                        if r < 0.5:
-                            service_req_domain[domain][reqID] = 'serve'
-                            num_serve += 1
-                            bytesSent_req_domain[domain][reqID] = None
-                        else:
-                            service_req_domain[domain][reqID] = 'queue'
-                            num_queue += 1
-                    bytesSent_req_domain[domain] = dict((k, v) for k, v in bytesSent_req_domain[domain].iteritems() if v)
-                    if len(bytesSent_req_domain[domain]) == 0:
-                        bytesSent_req_domain[domain] = None
-                bytesSent_req_domain = dict((k, v) for k, v in bytesSent_req_domain.iteritems() if v)
-                q.loc[idx, 'service_per_request_per_domain'] = json.dumps(service_req_domain)
-                if len(bytesSent_req_domain) == 0:
-                    self.q.drop([idx], inplace=True)
+    @staticmethod
+    def serve_row_(control_req, bytesSent_req_domain):
+        service_req_domain = {}
+        bytesSent_req_domain_updated = {}
+        num_req_serve = 0
+        num_req_queue = 0
+        for domain in bytesSent_req_domain:  # for each domain in the entry
+            for reqID in bytesSent_req_domain[domain]:  # for each request under the domain
+                if domain not in service_req_domain:
+                    service_req_domain[domain] = {}
+                r = np.random.rand()
+                if control_req == 'serve_all' or (control_req == 'random_serve_and_queue' and r<0.5):
+                    service_req_domain[domain][reqID] = 'serve'
+                    num_req_serve += 1
                 else:
-                    self.q.loc[idx, 'bytesSent_req_domain'] = json.dumps(bytesSent_req_domain)
+                    service_req_domain[domain][reqID] = 'queue'
+                    num_req_queue += 1
+                    # for queued requests, copy to byteSent dict as update
+                    if domain not in bytesSent_req_domain_updated:
+                        bytesSent_req_domain_updated[domain] = {}
+                    bytesSent_req_domain_updated[domain][reqID] = bytesSent_req_domain[domain][reqID]
+        return service_req_domain, bytesSent_req_domain_updated, num_req_serve, num_req_queue
 
-                # if multiple entries with same sessionID exist, merge into one entry
-                idx_old = (int(sessionID) == q['sessionID']).nonzero()[0][0]
-                if idx != idx_old:
-                    print idx,
-                    print idx_old
-                    service_req_domain_old = json.loads(q.loc[idx_old, 'service_per_request_per_domain'])
-                    for domain in service_req_domain:
-                        for reqID in service_req_domain[domain]:
-                            if domain not in service_req_domain_old:
-                                service_req_domain_old[domain] = {}
-                            service_req_domain_old[domain][reqID] = service_req_domain[domain][reqID]
-                    q.loc[idx_old, 'service_per_request_per_domain'] = json.dumps(service_req_domain_old)
-                    q.drop([idx], inplace=True)
-
-            if self.verbose > 0:
-                print "TrafficServer.__serve_requests__(): serving {} request and queueing {} requests.".format(num_serve, num_queue)
+    @staticmethod
+    def append_service_row(service_df, sessionID, service_req_domain):
+        flags = (int(sessionID) == service_df['sessionID'])
+        if len(flags.nonzero()[0]) == 0:  # no duplication
+            service_df = service_df.append(
+                {'sessionID': sessionID, 'service_per_request_per_domain': json.dumps(service_req_domain)},
+                ignore_index=True)
+        elif len(flags.nonzero()[0]) == 1:  # merge with existing row
+            idx_old = flags.nonzero()[0][0]
+            service_req_domain_old = json.loads(service_df.loc[idx_old, 'service_per_request_per_domain'])
+            for domain in service_req_domain:
+                for reqID in service_req_domain[domain]:
+                    if domain not in service_req_domain_old:
+                        service_req_domain_old[domain] = {}
+                    service_req_domain_old[domain][reqID] = service_req_domain[domain][reqID]
+            service_df.loc[idx_old, 'service_per_request_per_domain'] = json.dumps(service_req_domain_old)
         else:
-            if self.verbose > 0:
-                print "TrafficServer.__serve_requests__(): no control received, return empty service_df."
-
-        if q is not None:
-            service_df = pd.concat([service_df, q], axis=0, join='inner', ignore_index=True)
-
+            raise ValueError("More than one existing entry found in service_df!")
         return service_df
